ec61ccf47cc23f5b0f734c6c167194504e7b738e
Brendan Molloy <brendan@necessary.nu>
2023-08-25 16:03:02 +1000

    Re-add language changing

--- src/_templates/layouts/base.tsx
+++ src/_templates/layouts/base.tsx
@@ -11,18 +11,11 @@ export default function BasePage(page: PageData) {
         <meta charSet="utf-8" />
         <title>{title}</title>
         <link rel="stylesheet" href="/styles/index.css" />
-        {
-          /* {script(`
+        {script(`
           if (document.documentElement.hasAttribute("lang")) {
             localStorage.setItem("divvun:language", document.documentElement.getAttribute("lang"))
           }
-
-          const pagefind = await import("/pagefind/pagefind.js");
-          const search = await pagefind.search("North")
-          const fiveResults = await Promise.all(search.results.slice(0, 5).map(r => r.data()));
-          console.log(fiveResults);
-        `)} */
-        }
+        `)}
       </head>
       <body id="top">
         <Navbar url={originalUrl || url || "/"} />
