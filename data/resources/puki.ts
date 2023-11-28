465f070da513f0a43bb1be7e97fd78a692e0f57a
Sjur Moshagen <sjurnm@mac.com>
2023-11-28 23:03:58 +0200

    Add entry for Púki

---
+++ data/resources/puki.ts
@@ -0,0 +1,26 @@
+import { LinkType, Resource, ResourceType } from "~types/resource.ts"
+
+const resource: Resource = {
+  id: "puki",
+  type: ResourceType.External,
+  languages: ["is"],
+  category: "spellers",
+  name: {
+    en: "Púki spellchecker",
+    is: "Púki ritvilluvörn",
+  },
+  description: {
+    en: "Púki Icelandic spelling checker",
+    is: "Púki ritvilluvörn fyrir PC og Mac",
+  },
+  links: [
+    {
+      text: {
+        en: "Púki",
+      },
+      url: new URL("https://puki.is"),
+    },
+  ],
+}
+
+export default resource
