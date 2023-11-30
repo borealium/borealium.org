7ebe41725b55a1fdd6cf7891ff2be59bb4ccd61c
Sjur Moshagen <sjurnm@mac.com>
2023-11-30 04:56:40 +0200

    NDS for Kvääni

---
+++ data/resources/nds-fkv.ts
@@ -0,0 +1,29 @@
+import { LinkType, Resource, ResourceType } from "~types/resource.ts"
+
+const resource: Resource = {
+  id: "nds-fkv",
+  type: ResourceType.External,
+  languages: ["fkv"],
+  category: "dictionaries",
+  name: {
+    en: "NDS for Kvääni",
+  },
+  description: {
+    en: "Dictionary resources for Kvääni",
+  },
+  moreInfo: {
+    en: `
+      NDS — Nähttadigibáhko — for Kvääni contains dictionaries translating to and from Kvääni.
+    `,
+  },
+  links: [
+    {
+      text: {
+        en: "NDS Kvääni",
+      },
+      url: new URL("https://sanat.oahpa.no"),
+    },
+  ],
+}
+
+export default resource
