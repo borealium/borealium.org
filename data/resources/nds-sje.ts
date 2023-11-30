53bea5adf3898b43cabf630eb401c332e3a2281a
Sjur Moshagen <sjurnm@mac.com>
2023-11-30 04:26:26 +0200

    Add NDS for sje

---
+++ data/resources/nds-sje.ts
@@ -0,0 +1,29 @@
+import { LinkType, Resource, ResourceType } from "~types/resource.ts"
+
+const resource: Resource = {
+  id: "nds-sje",
+  type: ResourceType.External,
+  languages: ["sje"],
+  category: "dictionaries",
+  name: {
+    en: "NDS for Pite Sámi",
+  },
+  description: {
+    en: "Dictionary resources for Pite Sámi",
+  },
+  moreInfo: {
+    en: `
+      NDS — Nähttadigibáhko — for Pite Sámi contains dictionaries translating to and from Pite Sámi.
+    `,
+  },
+  links: [
+    {
+      text: {
+        en: "NDS Åarjel",
+      },
+      url: new URL("https://bahkogirrje.oahpa.no"),
+    },
+  ],
+}
+
+export default resource
