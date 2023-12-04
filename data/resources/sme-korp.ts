281276aeb139c1da205779584e25cddde86fa5b0
Sjur N Moshagen <sjurnm@mac.com>
2023-12-04 21:50:33 +0100

    Add sme-korp

---
+++ data/resources/sme-korp.ts
@@ -0,0 +1,24 @@
+import { LinkType, Resource, ResourceType } from "~types/resource.ts"
+
+const resource: Resource = {
+  id: "sme-korp",
+  type: ResourceType.External,
+  languages: ["se"],
+  category: "korp",
+  name: {
+    en: "Korp for North Sámi",
+  },
+  description: {
+    en: "In Korp for North Sámi you can search North Sámi texts. The text corpus is gramatically annotated.",
+  },
+  links: [
+    {
+      text: {
+        en: "Korp for North Sámi",
+      },
+      url: new URL("https://gtweb.uit.no/korp/#?lang=en"),
+    },
+  ],
+}
+
+export default resource
