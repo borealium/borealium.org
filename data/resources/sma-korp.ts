7dd88e3c376a6770b8301aaf180a745a3000585d
Sjur N Moshagen <sjurnm@mac.com>
2023-12-04 21:55:01 +0100

    SMA Korp

---
+++ data/resources/sma-korp.ts
@@ -0,0 +1,24 @@
+import { LinkType, Resource, ResourceType } from "~types/resource.ts"
+
+const resource: Resource = {
+  id: "sma-korp",
+  type: ResourceType.External,
+  languages: ["sma"],
+  category: "korp",
+  name: {
+    en: "Korp for South Sámi",
+  },
+  description: {
+    en: "In Korp for South Sámi you can search South Sámi texts. The text corpus is gramatically annotated.",
+  },
+  links: [
+    {
+      text: {
+        en: "Korp for South Sámi",
+      },
+      url: new URL("https://gtweb.uit.no/korp/?mode=sma#?lang=en"),
+    },
+  ],
+}
+
+export default resource
