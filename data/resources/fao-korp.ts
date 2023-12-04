693eb4d5d98299c4613eb0c7f2c55dfb7cf510c3
Sjur N Moshagen <sjurnm@mac.com>
2023-12-04 21:39:01 +0100

    FAO Korp

---
+++ data/resources/fao-korp.ts
@@ -0,0 +1,24 @@
+import { LinkType, Resource, ResourceType } from "~types/resource.ts"
+
+const resource: Resource = {
+  id: "fao-korp",
+  type: ResourceType.External,
+  languages: ["fo"],
+  category: "korp",
+  name: {
+    en: "Korp for Faroese",
+  },
+  description: {
+    en: "In the text corpus Korp, you can search for Faroese texts. The text corpus is gramatically annotated.",
+  },
+  links: [
+    {
+      text: {
+        en: "Korp for Faroese",
+      },
+      url: new URL("https://gtweb.uit.no/f_korp/?mode=fao#?lang=en"),
+    },
+  ],
+}
+
+export default resource
