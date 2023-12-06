b04b29cd20a9d4c41344b06bcaf4d8a6082c8fcb
Sjur N Moshagen <sjurnm@mac.com>
2023-12-06 06:59:56 +0100

    Online spellers

---
+++ data/resources/mul-online-speller-others.ts
@@ -0,0 +1,24 @@
+import { LinkType, Resource, ResourceType } from "~types/resource.ts"
+
+const resource: Resource = {
+  id: "online-speller-others",
+  type: ResourceType.External,
+  languages: ["fo", "kl", "sje", "fkv", "fit"],
+  category: "spellers",
+  name: {
+    en: "Online spellchecker",
+  },
+  description: {
+    en: "Online spelling checker for many languages",
+  },
+  links: [
+    {
+      text: {
+        en: "Online speller",
+      },
+      url: new URL("https://divvun.org/proofing/online-speller.html"),
+    },
+  ],
+}
+
+export default resource
---
+++ data/resources/mul-online-speller-sami.ts
@@ -0,0 +1,24 @@
+import { LinkType, Resource, ResourceType } from "~types/resource.ts"
+
+const resource: Resource = {
+  id: "online-speller-sami",
+  type: ResourceType.External,
+  languages: ["se", "sma", "smj", "smn", "sms"],
+  category: "spellers",
+  name: {
+    en: "Online Sámi spellchecker",
+  },
+  description: {
+    en: "Online spelling checker for the Sámi languages",
+  },
+  links: [
+    {
+      text: {
+        en: "Online speller",
+      },
+      url: new URL("https://divvun.no/korrektur/speller-demo.html"),
+    },
+  ],
+}
+
+export default resource
