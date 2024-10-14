1e97a301339ee3654c8ab1e522bd2118c1e0317c
Børre Gaup <albbas@gmail.com>
2024-10-14 21:09:37 +0200

    Needed when compiling resources

---
+++ data/resources.ts
@@ -0,0 +1,23 @@
+import { parse as tomlParse } from "std/toml/mod.ts"
+import { L10nPath } from "~types/l10n.ts"
+import { LangTag } from "~types/language.ts"
+import { fluentBundle, message } from "~plugins/fluent.ts"
+
+export function getL10NLanguages(resourceLang: string): string[] {
+  const tomlContent = Deno.readTextFileSync(`${Deno.cwd()}/${resourceLang}-l10n.toml`)
+  const toml = tomlParse(tomlContent) as Record<string, L10nPath[]>
+  return toml.paths[0].locales
+}
+
+export function makeResourceTranslations(
+  key: string,
+  resourceLang: string,
+  languages: LangTag[],
+): Record<LangTag, string> {
+  return languages.reduce((acc, language) => {
+    const bundle = fluentBundle(`${resourceLang}-resources`, language)
+    acc[language] = message(bundle, null, `~${resourceLang}-resources`, key)
+
+    return acc
+  }, {} as Record<LangTag, string>)
+}
