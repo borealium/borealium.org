329b4a757af192a63951fb99cb3b7dbfe5a5487a
Børre Gaup <albbas@gmail.com>
2024-10-24 13:26:22 +0200

    Add description for language (translatable)

--- src/_includes/language-index.tsx
+++ src/_includes/language-index.tsx
@@ -28,6 +28,9 @@ export default function LanguageIndexLayout(page: PageData & LanguageIndexProps 
           <h1 title={lang_t(languageId)}>
             {lang_t(languageId)}
           </h1>
+          <p>
+            {lang_t(languageId + "-description")}
+          </p>
         </div>
         <div className="search-page-results" data-pagefind-ignore>
           {resources.length === 0 && (
