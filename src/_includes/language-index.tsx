1669edbda9ecbd54e61d49c0a29ec265e53c1542
Børre Gaup <albbas@gmail.com>
2024-10-30 13:06:32 +0100

    Language resources by category

--- src/_includes/language-index.tsx
+++ src/_includes/language-index.tsx
@@ -4,6 +4,7 @@ import { autonym, selectLocale } from "~plugins/language-data.ts"
 import { CategoryLabel } from "~/_components/label.tsx"
 import { Markdown } from "~/_includes/markdown.ts"
 import { FluentPage } from "~plugins/fluent.ts"
+import { CategoryId } from "~types/category.ts"
 
 export const layout = "base.tsx"
 
@@ -15,6 +16,15 @@ type LanguageIndexProps = {
 export default function LanguageIndexLayout(page: PageData & LanguageIndexProps & FluentPage) {
   const { resources, lang, languageId, t } = page
   const lang_t = page.fluentBundle(page.lang, "languages")
+  const category_t = page.fluentBundle(page.lang, "categories")
+  const res_by_category: Record<CategoryId, [Resource]> = resources.reduce((acc, resource) => {
+    if (resource.category in acc) {
+      acc[resource.category].push(resource)
+    } else {
+      acc[resource.category] = [resource]
+    }
+    return acc
+  }, {} as Record<CategoryId, [Resource]>)
 
   return (
     <div
@@ -41,22 +51,32 @@ export default function LanguageIndexLayout(page: PageData & LanguageIndexProps 
               {t("no-resources-in-category")}
             </div>
           )}
-          {resources.map((resource) => {
-            const resName = selectLocale(lang, resource.name)
-            const resDescription = selectLocale(lang, resource.description)
-
+          {Object.entries(res_by_category).map(([category, resources]) => {
             return (
-              <li className="search-result">
-                <a href={`/resource/${resource.id}`}>
-                  <img src={"/static/images/tag-resource.svg"} alt="" />
-                  {resName}
-                </a>
-                {resDescription && (
-                  <Markdown as="p" className="description">
-                    {resDescription}
-                  </Markdown>
-                )}
-              </li>
+              <div key={category} className="category">
+                <h2>
+                  {category_t(category)}
+                </h2>
+                <ul>
+                  {resources.map((resource) => {
+                    const resName = selectLocale(lang, resource.name)
+                    const resDescription = selectLocale(lang, resource.description)
+                    return (
+                      <li className="search-result">
+                        <a href={`/resource/${resource.id}`}>
+                          <img src={"/static/images/tag-resource.svg"} alt="" />
+                          {resName}
+                        </a>
+                        {resDescription && (
+                          <Markdown as="p" className="description">
+                            {resDescription}
+                          </Markdown>
+                        )}
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
             )
           })}
         </div>
