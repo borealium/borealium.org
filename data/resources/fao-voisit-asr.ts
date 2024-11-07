98dba29b40c704ce64e6f578293fc291447b4b00
Sjur N Moshagen <sjurnm@mac.com>
2024-11-07 19:04:00 +0200

    Use same link construct as other resources

--- data/resources/fao-voisit-asr.ts
+++ data/resources/fao-voisit-asr.ts
@@ -3,6 +3,20 @@ import { getL10NLanguages, makeResourceTranslations } from "~data/resources.ts"
 
 const id = "voisit-asr"
 const resourceLang = "fao"
+const halfLinks = [
+  {
+    type: LinkType.Normal,
+    url: new URL("https://voisit.fo"),
+  },
+  {
+    type: LinkType.AppleAppStore,
+    url: new URL("https://apps.apple.com/dk/app/voisit/id6459887023?l=da"),
+  },
+  {
+    type: LinkType.GooglePlayStore,
+    url: new URL("https://play.google.com/store/apps/details?id=net.isolveit.voisit"),
+  },
+]
 
 const l10nLanguages = getL10NLanguages(resourceLang)
 
@@ -13,19 +27,7 @@ const resource: Resource = {
   category: "speech-recognition",
   name: makeResourceTranslations(`${id}`, resourceLang, l10nLanguages),
   description: makeResourceTranslations(`${id}-description`, resourceLang, l10nLanguages),
-  links: [
-    {
-      url: new URL("https://voisit.fo"),
-    },
-    {
-      type: LinkType.AppleAppStore,
-      url: new URL("https://apps.apple.com/dk/app/voisit/id6459887023?l=da"),
-    },
-    {
-      type: LinkType.GooglePlayStore,
-      url: new URL("https://play.google.com/store/apps/details?id=net.isolveit.voisit"),
-    },
-  ].map((halfLink, index) => {
+  links: halfLinks.map((halfLink, index) => {
     return {
       ...halfLink,
       text: makeResourceTranslations(`${id}-links-${index}`, resourceLang, l10nLanguages),
