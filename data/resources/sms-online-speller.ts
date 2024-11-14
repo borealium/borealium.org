e291a2d1f68fbb0dda1c68e2aef6b4977569b352
Sjur N Moshagen <sjurnm@mac.com>
2024-11-14 18:52:53 +0200

    ♻️ Online SMS speller is now a separate entry, with a deep link to the speller page with correct language selected

--- data/resources/sms-online-speller.ts
+++ data/resources/sms-online-speller.ts
@@ -1,22 +1,22 @@
 import { LinkType, Resource, ResourceType } from "~types/resource.ts"
 import { getL10NLanguages, makeResourceTranslations } from "~data/resources.ts"
 
-const id = "online-speller-sami"
-const resourceLang = "mul"
+const id = "sms-online-speller"
+const resourceLang = "sms"
 
 const l10nLanguages = getL10NLanguages(resourceLang)
 
 const halfLinks = [
   {
     type: LinkType.Normal,
-    url: new URL("https://divvun.no/korrektur/speller-demo.html"),
+    url: new URL(`https://divvun.no/korrektur/speller-demo.html?lang=${resourceLang}`),
   },
 ]
 
 const resource: Resource = {
   id,
   type: ResourceType.External,
-  languages: [],
+  languages: ["sms"],
   category: "spellers",
   name: makeResourceTranslations(`${id}`, resourceLang, l10nLanguages),
   description: makeResourceTranslations(`${id}-description`, resourceLang, l10nLanguages),
--- resources/en/sms-resources.ftl
+++ resources/en/sms-resources.ftl
@@ -10,3 +10,7 @@ sms-korp-links-0 = Korp for Skolt Sámi
 sms-oahpa = Oahpa for Skolt Sámi
 sms-oahpa-description = Learn Skolt Sami using a number of various exercises.
 sms-oahpa-links-0 = Oahpa nuõrti
+
+sms-online-speller = Skolt Sámi Online Spellchecker
+sms-online-speller-description = Online spellchecker for Skolt Sámi: paste in your text and get checks.
+sms-online-speller-links-0 = Nuõrti Online Speller
