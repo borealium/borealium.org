fe69bffcccbb6592f70fb0ae7d558c232577eebd
Sjur N Moshagen <sjurnm@mac.com>
2024-11-28 11:55:11 +0200

    Move online SJE speller to separate resource with direct link

--- data/resources/sje-online-speller.ts
+++ data/resources/sje-online-speller.ts
@@ -1,22 +1,22 @@
 import { LinkType, Resource, ResourceType } from "~types/resource.ts"
 import { getL10NLanguages, makeResourceTranslations } from "~data/resources.ts"
 
-const id = "online-speller-others"
-const resourceLang = "mul"
+const id = "sje-online-speller"
+const resourceLang = "sje"
 
 const l10nLanguages = getL10NLanguages(resourceLang)
 
 const halfLinks = [
   {
     type: LinkType.Normal,
-    url: new URL("https://divvun.org/proofing/online-speller.html"),
+    url: new URL(`https://divvun.org/proofing/online-speller.html?lang=${resourceLang}`),
   },
 ]
 
 const resource: Resource = {
   id,
   type: ResourceType.External,
-  languages: ["fkv", "fit"],
+  languages: ["sje"],
   category: "spellers",
   name: makeResourceTranslations(`${id}`, resourceLang, l10nLanguages),
   description: makeResourceTranslations(`${id}-description`, resourceLang, l10nLanguages),
--- resources/en/sje-resources.ftl
+++ resources/en/sje-resources.ftl
@@ -7,3 +7,7 @@ sjelex = Bidumsáme báhkogirrje
 sjelex-description = Pite Saami lexical database
 sjelex-more-info = The Pite Saami lexical database published on the web is mainly based on a collection of Pite Saami words from the project Insamling av pitesamiska ord (2008-2012; Nils-Henrik Bengtsson, Marianne Eriksson, Inger Fjällås, Eva-Karin Rosenberg, Gry Helen Sivertsen, Valborg Sjaggo, Dagny Skaile & Peter Steggo) with additions and extensive editing by Joshua Wilbur. Ongoing thanks also to Olve Utne and Peter Steggo for many additional corrections and additions.
 sjelex-links-0 = Bidumsáme báhkogirrje
+
+sje-online-speller = Pite Sámi Online Spellchecker
+sje-online-speller-description = Online spellchecker for Pite Sámi: paste in your text and get checks.
+sje-online-speller-links-0 = Bidum Online Speller
