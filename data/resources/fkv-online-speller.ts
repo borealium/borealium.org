df2924f5c3a77de41ce887390c5c6c3fcdba3d2b
Sjur N Moshagen <sjurnm@mac.com>
2024-11-28 14:10:47 +0200

    Move online FKV speller to separate resource with direct link

--- data/resources/fkv-online-speller.ts
+++ data/resources/fkv-online-speller.ts
@@ -1,22 +1,22 @@
 import { LinkType, Resource, ResourceType } from "~types/resource.ts"
 import { getL10NLanguages, makeResourceTranslations } from "~data/resources.ts"
 
-const id = "online-speller-others"
-const resourceLang = "mul"
+const id = "fkv-online-speller"
+const resourceLang = "fkv"
 
 const l10nLanguages = getL10NLanguages(resourceLang)
 
 const halfLinks = [
   {
     type: LinkType.Normal,
-    url: new URL("https://divvun.org/proofing/online-speller.html"),
+    url: new URL(`https://divvun.org/proofing/online-speller.html?lang=${resourceLang}`),
   },
 ]
 
 const resource: Resource = {
   id,
   type: ResourceType.External,
-  languages: ["fit"],
+  languages: ["fkv"],
   category: "spellers",
   name: makeResourceTranslations(`${id}`, resourceLang, l10nLanguages),
   description: makeResourceTranslations(`${id}-description`, resourceLang, l10nLanguages),
--- resources/en/fkv-resources.ftl
+++ resources/en/fkv-resources.ftl
@@ -6,3 +6,7 @@ nds-fkv-links-0 = NDS Kvääni
 fkv-korp = Korp for Kven
 fkv-korp-description = In Korp for Kven you can search Kven texts. The text corpus is grammatically annotated.
 fkv-korp-links-0 = Korp for Kven
+
+fkv-online-speller = Kven Online Spellchecker
+fkv-online-speller-description = Online spellchecker for Kven: paste in your text and get checks.
+fkv-online-speller-links-0 = Kven Online Speller
