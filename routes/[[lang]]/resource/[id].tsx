f1314ddb7875f080ce4d381fb7bf560dd48a6bcb
Dylan Hand <dylan.hand@gmail.com>
2026-07-09 12:07:31 +0200

    Extra default height on keyboard embed
    Fixes issue where, when browser has js disabled, keyboard was cut off

--- routes/[[lang]]/resource/[id].tsx
+++ routes/[[lang]]/resource/[id].tsx
@@ -216,7 +216,7 @@ function KeyboardLayoutEmbed({
       <iframe
         src={src}
         width="100%"
-        height="400"
+        height="460"
         loading="lazy"
         style={{ maxWidth: "900px", border: "none" }}
       />
